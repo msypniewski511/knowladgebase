f36937e3062e0e15167f45bb3c8ad5918272bb10
Maciej <msypniewski511@gmail.com>
2022-11-20 14:59:24 +0000

    Nonsens

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.1"
 
 set :application, "knowladgebase"
-set :repo_url, "git@github.com:msypniewski511/markdown.git"
+set :repo_url, "git@github.com:msypniewski511/knowladgebase.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
